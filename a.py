65f82d9037f63223379fd620a636a054368d90cf
amithsoddy <anithasoddy@gmail.com>
2024-10-27 18:12:52 +0530

    file handling

---
+++ a.py
@@ -0,0 +1,3 @@
+file=open('a.txt','a')
+file.write("worlds most dangerous trio yuji nishida,yuki ishikawa")
+file.close()
---
+++ w.py
@@ -0,0 +1,3 @@
+file=open('a.txt','w')
+file.write("lionel messi is a goat of footballer he is an argentinian player lionel messi won the 7 ballondors and the world cup in 2022")
+file.close()
